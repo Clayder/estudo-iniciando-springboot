754b4e90809388a269ec41fe09309705fddf0d08
Peter Clayder <peterclayder@gmail.com>
2018-12-01 20:19:03 -0200

    Primeiro controller

--- src/main/java/com/clayder/spring_app/App.java
+++ src/main/java/com/clayder/spring_app/App.java
@@ -1,13 +1,14 @@
 package com.clayder.spring_app;
 
-/**
- * Hello world!
- *
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+    public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
 }
